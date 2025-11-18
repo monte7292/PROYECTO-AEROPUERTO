e4b7af6f4c1a909987837f7d389fe5188c091059
DESKTOP-3LHBG1B\usuario_tarde <alejandromonterodeltoro@gmail.com>
2025-11-18 17:55:54 +0100

    first commit

---
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/AeropuertoApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.gonzalomariomontero.aeropuerto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeropuertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeropuertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/AeropuertoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.gonzalomariomontero.aeropuerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeropuertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
