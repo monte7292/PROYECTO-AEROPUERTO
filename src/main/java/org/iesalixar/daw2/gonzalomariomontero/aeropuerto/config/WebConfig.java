70b9d767d67b32e5cb197c44945df32826e6936f
DESKTOP-3LHBG1B\usuario_tarde <alejandromonterodeltoro@gmail.com>
2025-11-20 14:48:38 +0100

    WebConfig Terminado

---
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/config/WebConfig.java
@@ -0,0 +1,32 @@
+package org.iesalixar.daw2.gonzalomariomontero.aeropuerto.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    // Logger para registrar eventos importantes
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Obtener la variable UPLOAD_PATH desde las propiedades del sistema
+        String uploadPath = System.getProperty("UPLOAD_PATH");
+        // Verificar si la variable UPLOAD_PATH está configurada
+        if (uploadPath != null && !uploadPath.isEmpty()) {
+            logger.info("UPLOAD_PATH configurado correctamente: {}", uploadPath);
+            // Configurar Spring para servir archivos desde la ruta obtenida
+            //Cuando el servidor reciba una solicitud que coincida con/uploads/**,
+            // buscará el archivo en la ruta física uploadPath del sistema de archivos del servidor.
+            registry.addResourceHandler("/uploads/**")
+                    .addResourceLocations("file:" + uploadPath + "/");
+        } else {
+            logger.error("La variable de entorno UPLOAD_PATH no está configurada o está vacía.");
+        }
+    }
+}
+
+
