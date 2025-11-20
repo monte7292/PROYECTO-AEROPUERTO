dd373c39d68c789b1826f169c41d7ad8efd87e35
DESKTOP-3LHBG1B\usuario_tarde <alejandromonterodeltoro@gmail.com>
2025-11-20 16:45:03 +0100

    Terminada TicketRepository

---
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/repositories/TicketRepository.java
@@ -0,0 +1,10 @@
+package org.iesalixar.daw2.gonzalomariomontero.aeropuerto.repositories;
+import org.iesalixar.daw2.gonzalomariomontero.aeropuerto.entities.Ticket;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    Page<Ticket> findAll(Pageable pageable);
+}
+
