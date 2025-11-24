93c7bef06f7117e8f3449277dee89ee6e7c94e27
MMD1358 <xdeadx1358@gmail.com>
2025-11-24 15:53:07 +0100

    Repositorio de trabajador

---
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/repositories/TrabajadorRepository.java
@@ -0,0 +1,12 @@
+package org.iesalixar.daw2.gonzalomariomontero.aeropuerto.repositories;
+
+import org.iesalixar.daw2.gonzalomariomontero.aeropuerto.entities.Trabajador;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrabajadorRepository extends JpaRepository<Trabajador, Long> {
+    Page<Trabajador> findAll(Pageable pageable);
+
+    // Resto de métodos que vayamos considerando hacer
+}
