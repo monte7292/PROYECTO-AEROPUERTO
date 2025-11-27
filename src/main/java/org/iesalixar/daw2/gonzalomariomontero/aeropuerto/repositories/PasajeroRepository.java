1055abf6e25d741298cc96b41e69d3b5c6480c98
Programagons <programagons@gmail.com>
2025-11-27 16:30:19 +0100

    Controller AvionPasajero

--- src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/repositories/PasajeroRepository.java
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/repositories/PasajeroRepository.java
@@ -4,9 +4,20 @@ import org.iesalixar.daw2.gonzalomariomontero.aeropuerto.entities.Pasajero;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PasajeroRepository extends JpaRepository<Pasajero, Long> {
     Page<Pasajero> findAll(Pageable pageable);
+
+    Page<Pasajero> findByDocumento(String documento, Pageable pageable);
+
+    List<Pasajero> findPasajeroByDocumento(String documento);
+
+    @Query("SELECT COUNT(p) > 0 FROM Pasajero p WHERE p.documento = :documento")
+    boolean existsByDocumento(@Param("documento") String documento);
 
     // Resto de métodos que vayamos considerando hacer
 }
