4188a076b88a37d017ba48286893af823f0f4459
MMD1358 <xdeadx1358@gmail.com>
2025-11-27 16:28:14 +0100

    Controlador trabajador y pequeño fix en controlador ticket

---
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/controllers/TrabajadorController.java
@@ -0,0 +1,154 @@
+package org.iesalixar.daw2.gonzalomariomontero.aeropuerto.controllers;
+
+import org.iesalixar.daw2.gonzalomariomontero.aeropuerto.entities.Trabajador;
+import org.iesalixar.daw2.gonzalomariomontero.aeropuerto.repositories.AvionRepository;
+import org.iesalixar.daw2.gonzalomariomontero.aeropuerto.repositories.TrabajadorRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+
+/**
+ * Controlador que maneja las operaciones CRUD para la entidad `Trabajador`.
+ */
+@Controller
+@RequestMapping("/trabajadores")
+public class TrabajadorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TrabajadorController.class);
+
+    // DAO para gestionar las operaciones de los trabajadores en la base de datos.
+    @Autowired
+    private TrabajadorRepository trabajadorRepository;
+
+    @Autowired
+    private AvionRepository avionRepository;
+
+    /**
+     * Lista todos los trabajadores y los pasa como atributo al modelo para que sean
+     * accesibles en la vista `trabajador.html`.
+     *
+     * @param model Objeto del modelo para pasar datos a la vista.
+
+     * @return El nombre de la plantilla Thymeleaf para renderizar la lista de trabajadores.
+     */
+    @GetMapping()
+    public String listTrabajadores(@RequestParam(defaultValue = "1") int page, @RequestParam(required = false) String search, @RequestParam(required = false) String sort, Model model) {
+        logger.info("Solicitando la lista de todos los trabajadores..." + search);
+        Pageable pageable = PageRequest.of(page - 1, 5, getSort(sort));
+        Page<Trabajador> trabajador;
+        int totalPages = 0;
+        trabajador = trabajadorRepository.findAll(pageable);
+        totalPages = (int) Math.ceil((double) trabajadorRepository.count() / 5);
+        logger.info("Se han cargado {} trabajadores.", trabajador.toList().size());
+        model.addAttribute("listTrabajadores", trabajador.toList()); // Pasar la lista de trabajadores al modelo
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("search", search);
+        model.addAttribute("sort", sort);
+        return "trabajador"; // Nombre de la plantilla Thymeleaf a renderizar
+    }
+
+    /**
+     * Muestra el formulario para crear un nuevo trabajador.
+     *
+     * @param model Modelo para pasar datos a la vista.
+     * @return El nombre de la plantilla Thymeleaf para el formulario.
+     */
+    @GetMapping("/new")
+    public String showNewForm(Model model) {
+        logger.info("Mostrando formulario para nuevo trabajador.");
+        model.addAttribute("trabajador", new Trabajador());
+        model.addAttribute("aviones", avionRepository.findAll()); // Agregar lista de aviones para elegir
+        return "trabajador-form";
+    }
+
+    @GetMapping("/edit")
+    public String showEditForm(@RequestParam("id") Long id, Model model) {
+        logger.info("Mostrando formulario de edición para el trabajador con ID {}", id);
+        Optional<Trabajador> trabajadorOpt = trabajadorRepository.findById(id);
+        if (trabajadorOpt.isPresent()) {
+            logger.warn("No se encontró el trabajador con ID {}", id);
+            model.addAttribute("errorMessage", "No se encontró el trabajador.");
+        } else {
+            model.addAttribute("trabajador", trabajadorOpt);
+        }
+        model.addAttribute("aviones", avionRepository.findAll()); // Agregar lista de aviones para elegir
+        return "trabajador-form";
+    }
+
+    /**
+     * Inserta un nuevo trabajador en la base de datos.
+     *
+     * @param trabajador Objeto que contiene los datos del formulario.
+     * @param redirectAttributes Atributos para mensajes flash de redirección.
+     * @return Redirección a la lista de trabajadores.
+     */
+    @PostMapping("/insert")
+    public String insertTrabajador(@ModelAttribute("trabajador") Trabajador trabajador, RedirectAttributes redirectAttributes) {
+        logger.info("Insertando nuevo trabajador con nombre y apellido {}", trabajador.getNombre(), trabajador.getApellidos());
+
+        trabajadorRepository.save(trabajador);
+        logger.info("Trabajador {} insertado con éxito.", trabajador.getNombre(), trabajador.getApellidos());
+        return "redirect:/trabajadores"; // Redirigir a la lista de trabajadores
+    }
+
+    /**
+     * Actualiza un trabajador existente en la base de datos.
+     *
+     * @param trabajador Objeto que contiene los datos del formulario.
+     * @param redirectAttributes Atributos para mensajes flash de redirección.
+     * @return Redirección a la lista de trabajadores.
+     */
+    @PostMapping("/update")
+    public String updateTrabajador(@ModelAttribute("trabajador") Trabajador trabajador, RedirectAttributes redirectAttributes) {
+        logger.info("Actualizando trabajador con ID {}", trabajador.getNombre(), trabajador.getApellidos());
+
+        trabajadorRepository.save(trabajador);
+        logger.info("Trabajador con ID {} actualizado con éxito.", trabajador.getNombre(), trabajador.getApellidos());
+        return "redirect:/trabajadores"; // Redirigir a la lista de trabajadores
+    }
+
+    /**
+     * Elimina un trabajador de la base de datos.
+     *
+     * @param id ID del trabajador a eliminar.
+     * @param redirectAttributes Atributos para mensajes flash de redirección.
+     * @return Redirección a la lista de trabajadores.
+     */
+    @PostMapping("/delete")
+    public String deleteTrabajador(@RequestParam("id") Long id, RedirectAttributes redirectAttributes) {
+        logger.info("Eliminando trabajador con ID {}", id);
+        trabajadorRepository.deleteById(id);
+        logger.info("Trabajador con ID {} eliminado con éxito.", id);
+        return "redirect:/trabajadores"; // Redirigir a la lista de trabajadores
+    }
+
+    private Sort getSort(String sort) {
+        if (sort == null) {
+            return Sort.by("id").ascending();
+        }
+        return switch (sort) {
+            case "nombreAsc" -> Sort.by("nombre").ascending();
+            case "nombreDesc" -> Sort.by("nombre").descending();
+            case "apellidosAsc" -> Sort.by("apellidos").ascending();
+            case "apellidosDesc" -> Sort.by("apellidos").descending();
+            case "cargoAsc" -> Sort.by("cargo").ascending();
+            case "cargoDesc" -> Sort.by("cargo").descending();
+            case "fechaContratacionAsc" -> Sort.by("fechaContratacion").ascending();
+            case "fechaContratacionDesc" -> Sort.by("fechaContratacion").descending();
+            case "idDesc" -> Sort.by("id").descending();
+            default -> Sort.by("id").ascending();
+        };
+    }
+}
+
