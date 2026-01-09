35dbf161725a00da1dfe2a9892aeb2312168e18f
Programagons <programagons@gmail.com>
2026-01-09 21:21:55 +0100

    Autenticación JDBC

--- src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/config/SecurityConfig.java
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package org.iesalixar.daw2.gonzalomariomontero.aeropuerto.config;
 
+import org.iesalixar.daw2.gonzalomariomontero.aeropuerto.services.CustomUserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,8 +25,14 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
+
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+
+
     /**
      * Configura el filtro de seguridad para las solicitudes HTTP, especificando
      las
@@ -69,6 +79,13 @@ public class SecurityConfig {
                             .deleteCookies("JSESSIONID")
                             .permitAll();
                 })
+                .oauth2Login(oauth2 -> {
+                    logger.debug("Configurando login con OAuth2");
+                    oauth2
+                            .loginPage("/login") // Reutiliza la página de inicio de sesión personalizada
+                            .defaultSuccessUrl("/", true) // Redirige al inicio después del login exitoso con OAuth2
+                            .permitAll();
+                })
                 .sessionManagement(session -> {
                     logger.debug("Configurando política de gestión de sesiones");
 
@@ -78,37 +95,21 @@ public class SecurityConfig {
         return http.build();
     }
     /**
-     * Configura los detalles de usuario en memoria para pruebas y desarrollo,
-     asignando
-     * roles específicos a cada usuario.
+     * Configura el proveedor de autenticación para usar el servicio de detalles de
+     usuario
+     * personalizado y el codificador de contraseñas.
      *
-     * @return una instancia de {@link UserDetailsService} que proporciona
-    autenticación en memoria.
+     * @return una instancia de {@link DaoAuthenticationProvider} para la
+    autenticación.
      */
     @Bean
-    public UserDetailsService userDetailsService() {
-        logger.info("Entrando en el método userDetailsService");
-        logger.debug("Creando usuario con rol USER");
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-        logger.debug("Creando usuario con rol ADMIN");
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("password"))
-                .roles("ADMIN")
-                .build();
-        logger.debug("Creando usuario con rol MANAGER");
-        UserDetails manager = User.builder()
-                .username("manager")
-                .password(passwordEncoder().encode("password"))
-                .roles("MANAGER")
-                .build();
-        logger.info("Saliendo del método userDetailsService");
-        return new InMemoryUserDetailsManager(user, admin, manager);
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
+
     /**
      * Configura el codificador de contraseñas para cifrar las contraseñas de
      los usuarios
