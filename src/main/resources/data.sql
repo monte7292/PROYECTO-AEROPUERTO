7ad08cb3cdad470366dfcc2df5cd4212190b278d
Programagons <programagons@gmail.com>
2026-01-09 21:01:00 +0100

    Autenticación en memoria.
    - Dependencia agregada al pom
    - SecurityConfig creado
    - Datos en tabla e inserciones
    - Botón para login y logout en header.html
    - Login personalizado y con traducción
    - Páginas para errores personalizados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -222,3 +222,41 @@ VALUES (1, 1),
        (18, 18),
        (19, 19),
        (20, 20);
+
+
+------------------------------------------------------------
+-- USUARIOS
+------------------------------------------------------------
+
+
+-- Insertar datos de ejemplo para 'roles'
+INSERT
+IGNORE INTO roles (id, name) VALUES
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_MANAGER'),
+(3, 'ROLE_USER');
+-- Insertar datos de ejemplo para 'users'. La contraseña de cada usuario es password
+INSERT
+IGNORE INTO users (id, username, password, enabled, first_name,
+last_name, created_date, last_modified_date, last_password_change_date)
+VALUES
+(1, 'admin', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye',
+true, 'Admin', 'User', NOW(), NOW(), NOW()),
+(2, 'manager', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye',
+true, 'Manager', 'User', NOW(), NOW(), NOW()),
+(3, 'normal', '$2b$12$FVRijCavVZ7Qt15.CQssHe9m/6eLAdjAv0PiOKFIjMU161wApxzye',
+true, 'Regular', 'User', NOW(), NOW(), NOW());
+
+
+-- Asignar el rol de administrador al usuario con id 1
+INSERT
+IGNORE INTO user_roles (user_id, role_id) VALUES
+(1, 1);
+-- Asignar el rol de gestor al usuario con id 2
+INSERT
+IGNORE INTO user_roles (user_id, role_id) VALUES
+(2, 2);
+-- Asignar el rol de usuario normal al usuario con id 3
+INSERT
+IGNORE INTO user_roles (user_id, role_id) VALUES
+(3, 3);
