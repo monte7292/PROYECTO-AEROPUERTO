474eedf8c78561f8921f2a0158ab79a6bcfe5b97
Programagons <programagons@gmail.com>
2026-01-09 21:22:59 +0100

    Autenticación Ouath (en proceso)

--- src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/controllers/LoginController.java
+++ src/main/java/org/iesalixar/daw2/gonzalomariomontero/aeropuerto/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package org.iesalixar.daw2.gonzalomariomontero.aeropuerto.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
     @GetMapping("/login")
     public String login(Model model) {
+        return "login"; // Redirige a una plantilla personalizada de login
+    }
+
+    /**
+     * Maneja las solicitudes GET a la página de inicio de sesión.
+     * Recupera mensajes de error de la sesión, si existen, y los pasa al modelo
+     * para ser mostrados en la vista de login.
+     * @param request El objeto {@link HttpServletRequest} que contiene la solicitud
+    HTTP.
+     * @param model El objeto {@link Model} que se utiliza para pasar atributos a
+    la vista.
+     * @return El nombre de la plantilla de Thymeleaf que renderiza la página de
+    login.
+     */
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model) {
+        // Recuperar el mensaje de error desde la sesión
+        String errorMessage = (String)
+                request.getSession().getAttribute("errorMessage");
+        // Si existe un mensaje de error, añadirlo al modelo y limpiar la sesión
+        if (errorMessage != null) {
+            model.addAttribute("errorMessage", errorMessage);
+            request.getSession().removeAttribute("errorMessage"); // Limpiar el mensaje después de mostrarlo
+        }
         return "login"; // Redirige a una plantilla personalizada de login
     }
 }
